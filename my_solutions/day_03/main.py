2db5fe0cd6332c08e1d3df96f1ea8b0df351d057
appinha <amanda_pinha@hotmail.com>
2022-11-29 22:41:47 -0300

    [day_03] Updates files to conform to new framework

--- my_solutions/day_03/main.py
+++ my_solutions/day_03/main.py
@@ -1,20 +1,23 @@
-from puzzle_solver import PuzzleSolver, run_puzzle_solver
-from helpers import invert_binary, lst_to_str
+import sys; sys.path.insert(0, '..')
+import aoc_lib as lib
 from pprint import pprint
+
+from helpers import invert_binary, lst_to_str
 from collections import defaultdict, Counter
 
 
-delimiter = "\n"
+class DayPuzzleSolver():
+    def __init__(self):
+        self.delimiter = "\n"
 
+    def solve_part_1(self, raw_input):
+        gamma, epsilon = self._get_gamma_and_epsilon(raw_input)
+        return int(gamma, 2) * int(epsilon, 2)
 
-class DayPuzzleSolver(PuzzleSolver):
-    def __init__(self, input_file, delimiter):
-        PuzzleSolver.__init__(self, input_file, delimiter)
-
-    def get_input(self, raw_input):
-        self.gamma, self.epsilon = self._get_gamma_and_epsilon(raw_input)
-        self.o2 = self._get_rating(raw_input, 'o2')
-        self.co2 = self._get_rating(raw_input, 'co2')
+    def solve_part_2(self, raw_input):
+        o2 = self._get_rating(raw_input, 'o2')
+        co2 = self._get_rating(raw_input, 'co2')
+        return int(o2, 2) * int(co2, 2)
 
     def _get_bit_counts(self, raw_input):
         count_by_bit_by_position = defaultdict(lambda: Counter())
@@ -43,13 +46,3 @@ class DayPuzzleSolver(PuzzleSolver):
             binaries = [binary for binary in binaries if binary[position] == rate[position]]
             position += 1
         return binaries[0]
-
-    def solve_part_1(self):
-        return int(self.gamma, 2) * int(self.epsilon, 2)
-
-    def solve_part_2(self):
-        return int(self.o2, 2) * int(self.co2, 2)
-
-
-if __name__ == '__main__':
-    run_puzzle_solver(DayPuzzleSolver, delimiter)
