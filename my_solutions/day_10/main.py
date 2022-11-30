838b000b1dc80a3a040be55d8a881813c6503101
appinha <amanda_pinha@hotmail.com>
2022-11-30 17:38:50 -0300

    [day_10] Updates files to conform to new framework

--- my_solutions/day_10/main.py
+++ my_solutions/day_10/main.py
@@ -1,19 +1,20 @@
-from puzzle_solver import PuzzleSolver, run_puzzle_solver
+import sys; sys.path.insert(0, '..')
+import aoc_lib as lib
 from pprint import pprint
 
 
-delimiter = "\n"
-
-
-class DayPuzzleSolver(PuzzleSolver):
-    def __init__(self, input_file, delimiter):
-        PuzzleSolver.__init__(self, input_file, delimiter)
+class DayPuzzleSolver():
+    def __init__(self):
+        self.delimiter = "\n"
         self.char_pairs = {'(': ')', '[': ']', '{': '}', '<': '>'}
         self.error_score_by_char = {')': 3, ']': 57, '}': 1197, '>': 25137}
         self.complete_score_by_char = {')': 1, ']': 2, '}': 3, '>': 4}
 
-    def get_input(self, raw_input):
-        self.subsystem = raw_input
+    def solve_part_1(self, raw_input):
+        return self._get_error_score(raw_input)
+
+    def solve_part_2(self, raw_input):
+        return self._get_middle_score(raw_input)
 
     def _get_line_error_score(self, line):
         stack = []
@@ -47,20 +48,10 @@ class DayPuzzleSolver(PuzzleSolver):
         closing_chars = close_remaining(remaining_open_chars)
         return calculate_score(closing_chars)
 
-    def _get_error_score(self):
-        return sum(self._get_line_error_score(line) for line in self.subsystem)
+    def _get_error_score(self, subsystem):
+        return sum(self._get_line_error_score(line) for line in subsystem)
 
-    def _get_middle_score(self):
-        incomplete_lines = [line for line in self.subsystem if not self._get_line_error_score(line)]
+    def _get_middle_score(self, subsystem):
+        incomplete_lines = [line for line in subsystem if not self._get_line_error_score(line)]
         middle_scores = sorted([self._get_line_middle_score(line) for line in incomplete_lines])
         return middle_scores[len(middle_scores) // 2]
-
-    def solve_part_1(self):
-        return self._get_error_score()
-
-    def solve_part_2(self):
-        return self._get_middle_score()
-
-
-if __name__ == '__main__':
-    run_puzzle_solver(DayPuzzleSolver, delimiter)
